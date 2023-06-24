4dc938745f84dfa42ef5816c3947676cd30bc72e
sunly <sunjingxiang1995@gmail.com>
2023-06-24 21:02:33 +0800

    feat: 实现getters

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@ import { useStore } from "./vuex/index";
 const store = useStore("custom-store");
 
 const count = computed(() => store.state.count);
+const doubleCount = computed(() => store.getters.doubleCount);
 
 const handleEditState = () => {
   store.state.count += 1;
@@ -15,6 +16,7 @@ const handleEditState = () => {
   <div>
     <h1>vue3 + vuex demo</h1>
     <h3>count: {{ count }}</h3>
+    <h3>getter double count: {{ doubleCount }}</h3>
     <button @click="handleEditState">直接修改state(不合法)</button>
   </div>
 </template>
--- src/vuex/index.js
+++ src/vuex/index.js
@@ -1,11 +1,30 @@
 import { inject, reactive } from "vue";
 
 const DEFAULT_STORE_NAME = "defaultStore";
+const forEachObj = function (obj, fn) {
+  Object.keys(obj).forEach((key) => fn(obj[key], key));
+};
 
 class Store {
   constructor(options) {
     console.log("store实例数据", options);
+
+    // 添加state到store
     this._state = reactive({ data: options.state });
+
+    // 添加getter到store
+    this.getters = {};
+    let _this = this;
+    // 遍历getters对象，对象属性值都是函数，我们需要调用这些函数获取它们的返回值
+    // 使用defineProperty将函数都添加到store.getters对象上，之后外部调用getters时就会走到get属性访问器中，并执行函数
+    forEachObj(options.getters, (fn, key) => {
+      Object.defineProperty(this.getters, key, {
+        get() {
+          // return computed(() => fn(_this.state));
+          return fn(_this.state);
+        },
+      });
+    });
   }
   get state() {
     return this._state.data;
